c6a7b01bd641f15550da25993f999106f0e0f816
Shuai Wang <shuai.wang1@thoughtworks.com>
2020-04-07 12:59:33 +0800

    add new feature

--- src/main/java/cn/xpbootcamp/locker_robot/LockerRobot.java
+++ src/main/java/cn/xpbootcamp/locker_robot/LockerRobot.java
@@ -1,15 +1,11 @@
 package cn.xpbootcamp.locker_robot;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class LockerRobot {
-    private static int CAPACITY_OF_LOCKER = 5;
-    List<Locker> lockerList = new ArrayList<>();
-    public LockerRobot(int lockers) {
-        for (int i = 0; i < lockers; i++) {
-            lockerList.add(new Locker(CAPACITY_OF_LOCKER));
-        }
+    List<Locker> lockerList;
+    public LockerRobot(List<Locker> lockerList) {
+        this.lockerList = lockerList;
     }
 
     public Ticket save() {
--- src/test/java/cn/xpbootcamp/locker_robot/RobotTest.java
+++ src/test/java/cn/xpbootcamp/locker_robot/RobotTest.java
@@ -2,14 +2,25 @@ package cn.xpbootcamp.locker_robot;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RobotTest {
 
     @Test
     public void should_return_ticket_when_saving_given_first_locker_is_available_and_second_locker_is_available() {
-        LockerRobot lockerRobot = new LockerRobot(2);
-        assertNotNull(lockerRobot.save());
+        Locker locker1 = new Locker(1);
+        Locker locker2 = new Locker(1);
+        List<Locker> lockerList = new ArrayList<>();
+        lockerList.add(locker1);
+        lockerList.add(locker2);
+        LockerRobot lockerRobot = new LockerRobot(lockerList);
+
+        Ticket ticket = lockerRobot.save();
+        assertNotNull(ticket);
+        assertEquals(0, locker1.available);
     }
 
 
